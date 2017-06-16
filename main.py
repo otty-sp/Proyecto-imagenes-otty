759318fb4ce5389f8392c34d9d70c45fadd06e6a
otty-sp <otty.segurap@gmail.com>
2017-06-16 12:47:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,61 @@
-Ruta = "otty"
-ListaImagenes = []
-Para cada carpeta en Ruta hacer:
-  ListaDirectorios <=LeerDirectorio
-  si hay imagenes
-    para cada imagen hacer:
-        nuvImagen = Imagen("nombre",Ruta,"")
-        ListaImagenes.append(nuvImagen)
-  continuar a la siguiente carpeta
- 
- clasificador(listaImagenes)
+#crear lista de imagenes, guardar imagenes
+class Listaim:
+	def __init__(self):
+		self.lista=[]
+#almacenar en mas de una carpeta
+	def almacenar(self):
+		try
+		os.mkdir(os.path.dirname(os.path.realpath(__file__))+'/etiquetada')
+		except:
+			pass
+		 guardado=os.path.dirname(os.path.realpath(__file__))+'/etiquetada'
+		 informacion=open(guardado+'/'+'informacion.txt','a')
+		if os.path.exists(carpeta)==True:
+			if os.path.isdir(carpeta)==True:
+		for archivo in os.listdir(carpeta):
+			if os.path.isfile(carpeta+'/'+archivo)==True:
+				if archivo.lower().endswith(('.png')):
+					ver=Image.open(carpeta+'/'+archivo)
+					ver.show()
+					etiqueta=raw_input('Etiqueta ')
+					shutil.copy2(carpeta+'/'+archivo,guardado)
+					self.lista.append(Etiqueta(guardado+'/'+archivo,etiqueta))
+					informacion.write(archivo+' '+etiqueta+' '+'\n')
+					else:
+						pass
+					elif os.path.isdir(carpeta+'/'+archivo)==True:
+						self.almacenar(carpeta+'/'+archivo)
+					else:
+						pass
+			else:
+				print 'ruta dada no existe'
+		else:
+			print 'la carpeta no existe'
+		informacion.close()
+#contara las etiquedas de la carpeta del directorio		
+	def contar_etiquetas(self):
+		etiquetas={}
+		for i in self.lista:
+			if i.etiqueta in etiquetas:
+				etiquetas[i.etiqueta]+=1
+			else:
+				etiquetas[i.etiqueta]=1
+		return etiquetas
+
+		
+#definimos revisar para "revisar" las imagenes guardadas en la lista
+	def revisar(self):
+		if os.path.isdir(os.path.dirname(os.path.realpath(__file__))+'/etiqueta')==True:
+			if os.path.isfile(os.path.dirname(os.path.realpath(__file__))+'/etiquetada/informacion.txt'):
+				informacion=open(os.path.dirname(os.path.realpath(__file__))+'/etiquetada/informacion.txt','r')
+				for linea in informacion:
+					archivo,etiqueta,comentario=linea.split()
+					if os.path.isfile(os.path.dirname(os.path.realpath(__file__))+'/etiquetada/'+archivo):
+						self.lista.append(Etiqueta(os.path.dirname(os.path.realpath(__file__))+'/etiquetado/'+archivo,etiqueta,comentario))
+					else:
+						pass
+			else:
+				print 'falta archivo de informacion'
+		else:
+			pass
+	
