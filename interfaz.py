58d674365c4faa245ea0f68c9cdfe53ac81dfebf
otty-sp <otty.segurap@gmail.com>
2017-06-26 16:30:28 -0500

    Update interfaz.py

--- interfaz.py
+++ interfaz.py
@@ -4,34 +4,48 @@ from PIL import Image
 
 
 def tag(numID,tag,ob_img):
-for i in ob_img:
-if i.num == numID:
-i.tags.append(tag)
+  for i in ob_img:
+  if i.num == numID:
+  i.tags.append(tag)
 #Funcion que abre en el formato gif a una imagen
+/* Esta funcion requiere que le pases el num de imagen */
 def abrir_img(path_img):
-path_temp = path_img +".gif"
-try:
-Image.open(path_img).save(path_temp)
-except IOError:
-print("No se puede convertir la imagen")
-imagen1 = PhotoImage(file=path_temp)
-return imagen1
-def mostrar_interfaz(ob_img):
-num = buscar_avance(ob_img) 
+    path_temp = path_img +".gif"
+  try:
+    Image.open(path_img).save(path_temp)
+    except IOError:
+    print("No se puede convertir la imagen")
+    imagen1 = PhotoImage(file=path_temp)
+  return imagen1
+  
 
-ventana = Tk()
-ventana.title("Clasificador de Imagenes")
-ventana.config(bg="gray")
-ventana.geometry("600x700")
-Label = Label(ventana, image = abrir_img(ob_img[0].ruta))
-Label.grid(row=1,column=2)
-boton1 = button(ventana,"siguiente")
-boton1.button.grid(row=1,column=1)
-boton2 = button(ventana, "anterior")
-boton2.button.grid(row=1,column=2)
-boton3 = button(ventana, "finalizar")
-boton3.button.grid(row=1,column=3)
-ventana.mainloop()
+## Utiliza esta idea
+
+def getImg(self, img):
+        self.masterImg = Image.open(os.path.join("images",
+                                  self.images[img]))
+        self.masterImg.thumbnail((400, 400))
+        self.img = ImageTk.PhotoImage(self.masterImg)
+        self.lbl['image'] = self.img
+
+
+  
+def mostrar_interfaz(ob_img):
+  num = buscar_avance(ob_img) 
+
+  ventana = Tk()
+  ventana.title("Clasificador de Imagenes")
+  ventana.config(bg="gray")
+  ventana.geometry("600x700")
+  Label = Label(ventana, image = abrir_img(ob_img[0].ruta))
+  Label.grid(row=1,column=2)
+  boton1 = button(ventana,"siguiente")
+  boton1.button.grid(row=1,column=1)
+  boton2 = button(ventana, "anterior")
+  boton2.button.grid(row=1,column=2)
+  boton3 = button(ventana, "finalizar")
+  boton3.button.grid(row=1,column=3)
+  ventana.mainloop()
 
 ob_img = []
 path_carpeta = raw_input("introduce la direcion de la carpeta que contiene las imagenes")
